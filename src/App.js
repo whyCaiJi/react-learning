f654213329492f1b066d2b9c43891a3005068901
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2018-12-13 00:41:39 +0800

    Make a new component with props, make the component folder for better management

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Intro from './components/Intro';
 import './App.css';
+
+// Customized components to be capitalized, differentiating from in-built tags
+// Lesson 3: creating a new component
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">TV Series List</h1>
         </header>
+        <Intro message="Here you can find all of your most loved series" />
       </div>
     );
   }
