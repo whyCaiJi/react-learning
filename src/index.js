11ac4124c1c76a2458bfea799af145236368bf99
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2018-12-13 00:28:34 +0800

    Commented out previous lesson

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // Lesson 1: creating a react element
-const greeting = React.createElement('h1', {}, 'Hello World!');
+// const greeting = React.createElement('h1', {}, 'Hello World!');
 
 // Lesson 2: JSX
-const getCurrentDate = () => {	// Arrow functions
-	const date = new Date();
-	return date.toDateString();
-}
-const greeting1 = <h1>Hello World! Current Date: {getCurrentDate()}</h1>;
+// const getCurrentDate = () => {	// Arrow functions
+// 	const date = new Date();
+// 	return date.toDateString();
+// }
+// const greeting1 = <h1>Hello World! Current Date: {getCurrentDate()}</h1>;
 
 
 
 
-ReactDOM.render(greeting1, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
