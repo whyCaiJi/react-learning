8afd1b10f5f6ff6387b7512f12cdc8c6d0ff4dab
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2018-12-14 00:23:13 +0800

    Add an input feature into the app

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './index.css';
 
 
-const SeriesListItem = ( {series} ) => (
-	<li key={series.show.id}>
+const SeriesListItem = ({ series }) => (
+	<li>
 		{series.show.name}
 	</li>
 )
+
 const SeriesList = (props) => {
 	return (
 		<div>
 			<ul className="series-list">
 				{props.list.map(series => (
-					<SeriesListItem series={series}/>
+					<SeriesListItem series={series}  key={series.show.id}/>
 				))}
 			</ul>
 		</div>
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -7,18 +7,21 @@ class Series extends Component {
     	series: []
   	}
 
-  	// Lifecycle tutorial: needs to understand callback function
-  	// Async call to API and need to understand Promise
-  	componentDidMount() {
-    	fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+  onSeriesInputChange = e => {
+
+  	fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
       		.then(response => response.json())
       		.then(json => this.setState({ series: json }))
-  	}
+  }
+  
 
 	render() {
 		return (
 			<div>
 				The length of series array - {this.state.series.length}
+				<div>
+					<input type="text" onChange={this.onSeriesInputChange}/>
+				</div>
 				<SeriesList list={this.state.series} />
 			</div>
 		)
