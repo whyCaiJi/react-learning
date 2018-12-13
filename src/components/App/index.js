2d10cec47e96c1bd0e329ba1013641ffe15c3933
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2018-12-13 23:24:36 +0800

    This part can follow but need to understand how promise work and fetch works

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Intro from '../Intro';
 import './App.css';
+import 'whatwg-fetch';
 
 // Customized components to be capitalized, differentiating from in-built tags
 // Lesson 3: creating a new component
@@ -13,12 +14,11 @@ class App extends Component {
   }
 
   // Lifecycle tutorial: needs to understand callback function
+  // Async call to API and need to understand Promise
   componentDidMount() {
-    const series = ["Vikings", "Game of Thrones"];
-
-    setTimeout(() => {
-      this.setState({ series });
-    }, 2000);
+    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+      .then(response => response.json())
+      .then(json => this.setState({ series: json}))
   }
 
   render() {
